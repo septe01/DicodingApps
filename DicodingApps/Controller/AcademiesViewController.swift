254c03706aac9d1ed4e61aa088ec7ea353ca7206
septe01 <septe.vhirgo@gmail.com>
2021-07-09 20:39:00 +0700

    creating IBoutleat per Item in chalenge and academy and create connect use controller to detail with sending image

--- DicodingApps/Controller/AcademiesViewController.swift
+++ DicodingApps/Controller/AcademiesViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class AcademiesViewController: UIViewController {
 
+    @IBOutlet weak var androidAcademy: UIStackView!
+    @IBOutlet weak var unityAcademy: UIStackView!
+    @IBOutlet weak var cloudAcademy: UIStackView!
+    @IBOutlet weak var arvrAcademy: UIStackView!
+    
+    // Digunakan untuk menampung data item
+    var academies = [
+        ("Android Developer","lorem ipsum",UIImage(named: "android")),
+        ("Unity Game Developer","lorem ipsum",UIImage(named: "unity")),
+        ("Ar / Vr DEveloper","lorem ipsum",UIImage(named: "arvr")),
+        ("Cloud Developer","lorem ipsum",UIImage(named: "cloud")),
+        ("Lorem ipsum dolor sit amet", "Lorem ipsum", UIImage(named: "android"))
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Menambahkan aksi tapped pada stack view
+        addTapped(parameter: &androidAcademy)
+        addTapped(parameter: &unityAcademy)
+        addTapped(parameter: &cloudAcademy)
+        addTapped(parameter: &arvrAcademy)
+        
     }
+    
+    func addTapped(parameter: inout UIStackView) {
+        // Menambahkan gestur tap
+        parameter.isUserInteractionEnabled = true
+        parameter.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.objectTapped)))
+    }
+    
+    
+    @objc func objectTapped(gesture: UIGestureRecognizer){
+        // Digunakan menginisialisasi DetailViewController
+        guard let controller = UIStoryboard( // call mainstoryboard
+            name: "Main", bundle: nil
+        ).instantiateViewController(
+        withIdentifier: "DetailViewControllerScene"
+        )as? DetailViewController else {return}
+        
+        // Kode di bawah digunakan untuk menentukan stack view mana yang dipilih
+        switch gesture.view as? UIStackView {
+        case (androidAcademy):
+            controller.detail = academies[0]
+        case (unityAcademy):
+            controller.detail = academies[1]
+        case (arvrAcademy):
+            controller.detail = academies[2]
+        case (cloudAcademy):
+            controller.detail = academies[3]
+        default:
+            controller.detail = academies[4]
+        }
+        
+        // Kode dibawah digunakan untuk berpindah ke halaman detail dengan membawa informasi yang sesuai
+        self.present(controller, animated: true, completion: nil)
+        
+    }
+    
     
 
     /*
--- DicodingApps/Controller/DetailViewController.swift
+++ DicodingApps/Controller/DetailViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    // IBOutlets menghubungkan view dengan controller
+    @IBOutlet weak var titleDetail: UILabel!
+    @IBOutlet weak var imageDetail: UIImageView!
+    @IBOutlet weak var descDetail: UILabel!
+    
+    // Digunakan untuk menampung data sementara
+    var detail: (title:String?, desc:String?,image:UIImage?)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        titleDetail.text = detail.title
+        descDetail.text = detail.desc
+        imageDetail.image = detail.image
+        
     }
     
 
